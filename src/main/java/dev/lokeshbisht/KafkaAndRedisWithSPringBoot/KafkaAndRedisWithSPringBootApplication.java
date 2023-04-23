22d8d802e35462c7b96d3d14b1e22d78e22649f7
Lokesh-Bisht <lokeshbisht.096@gmail.com>
2023-04-23 20:16:13 +0530

    Initial commit

---
+++ src/main/java/dev/lokeshbisht/KafkaAndRedisWithSPringBoot/KafkaAndRedisWithSPringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.KafkaAndRedisWithSPringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAndRedisWithSPringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAndRedisWithSPringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lokeshbisht/KafkaAndRedisWithSPringBoot/KafkaAndRedisWithSPringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lokeshbisht.KafkaAndRedisWithSPringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAndRedisWithSPringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
