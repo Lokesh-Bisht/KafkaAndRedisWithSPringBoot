95f9e8a5e154e014f417df8637cf6c153dedbca5
Lokesh Bisht <lokeshbisht.096@gmail.com>
2024-01-01 23:43:09 +0530

    feat: #54

--- SongService/src/main/java/dev/lokeshbisht/SongService/service/SongService.java
+++ SongService/src/main/java/dev/lokeshbisht/SongService/service/SongService.java
@@ -1,5 +1,6 @@
 package dev.lokeshbisht.SongService.service;
 
+import dev.lokeshbisht.SongService.dto.song.request.SongPlayCountRequestDto;
 import dev.lokeshbisht.SongService.dto.song.request.SongRequestDto;
 import dev.lokeshbisht.SongService.dto.song.response.SongDto;
 
@@ -7,4 +8,5 @@ public interface SongService {
 
     SongDto addSong(SongRequestDto songRequestDto, Long genreId);
     SongDto updateSong(SongRequestDto songRequestDto, Long genreId, Long songId);
+    SongDto updatePlayCount(SongPlayCountRequestDto songPlayCountRequestDto, Long songId);
 }
