3a7a68a39a83b71d23338509493d43bd7bc60b9a
Lokesh Bisht <lokeshbisht.096@gmail.com>
2024-01-06 23:59:36 +0530

    Call create genre api

--- src/main/java/dev/lokeshbisht/KafkaAndRedisWithSpringBoot/consumers/GenreConsumer.java
+++ src/main/java/dev/lokeshbisht/KafkaAndRedisWithSpringBoot/consumers/GenreConsumer.java
@@ -1,17 +1,35 @@
 package dev.lokeshbisht.KafkaAndRedisWithSpringBoot.consumers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.lokeshbisht.KafkaAndRedisWithSpringBoot.dto.request.CreateGenreRequestDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.client.RestTemplate;
 
 public class GenreConsumer {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Value("${kafka.topic.genre}")
     private String genreTopic;
+
+    @Value("${kafka.service.baser-url}")
+    private String songServiceBaseUrl;
 
     private static final Logger logger = LoggerFactory.getLogger(GenreConsumer.class);
 
@@ -24,5 +42,16 @@ public class GenreConsumer {
     ) {
         logger.info("Consuming message from topic {} with partition {}, offset {}, timestamp: {}, and metadata: {}", genreTopic, partition, offset, timestamp, metadata);
         logger.info("Consumed message: " + message);
+        String url = songServiceBaseUrl + "/v1/genre";
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            CreateGenreRequestDto createGenreRequestDto = null;
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(createGenreRequestDto, headers), new ParameterizedTypeReference<>() {
+            });
+        } catch (Exception ex) {
+            logger.error("An error has occurred while calling POST {}", url);
+            logger.error("Exception: {}", ex.getMessage());
+        }
     }
 }
