a0c903dbcd64b0ff635de0aa6cba109ff65baed4
Lokesh Bisht <lokeshbisht.096@gmail.com>
2024-01-09 23:41:45 +0530

    Add album dto and entity

---
+++ SongService/src/main/java/dev/lokeshbisht/SongService/entity/Album.java
@@ -0,0 +1,46 @@
+package dev.lokeshbisht.SongService.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "album_type")
+    private String albumType;
+
+    @Column(name = "total_tracks")
+    private Integer totalTracks;
+
+    @Column(name = "available_markets")
+    private List<String> availableMarkets;
+
+    private String href;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+}
