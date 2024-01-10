9c061da0e41a6d0f2840633182dae4b83d09d1b4
Lokesh Bisht <lokeshbisht.096@gmail.com>
2024-01-10 22:31:09 +0530

    Add invalid resource exception

--- SongService/src/main/java/dev/lokeshbisht/SongService/enums/ErrorCode.java
+++ SongService/src/main/java/dev/lokeshbisht/SongService/enums/ErrorCode.java
@@ -2,6 +2,7 @@ package dev.lokeshbisht.SongService.enums;
 
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR,
+    INVALID_RESOURCE,
     SONG_NOT_FOUND,
     GENRE_NOT_FOUND,
     ALBUM_NOT_FOUND,
